aff4a5cfa2e63ca8e3825122c7d1d67914cbf7a4
AndreeaSichitiu <123585629+AndreeaSichitiu@users.noreply.github.com>
2023-04-09 08:21:15 +0000

    Deploying to gh-pages from @ AndreeaSichitiu/team-6-ice-project@cd97f0f83bef27172013e9d3f245f38a35766c3c 🚀

---
+++ index.b4ed9ec8.js
@@ -0,0 +1,2 @@
+nextButton.addEventListener("click",(()=>{selectedSlideIndex=selectedSlideIndex===slides.length-1?0:selectedSlideIndex+1,slides.map((function(e,d){e.style.display=d===selectedSlideIndex?"block":"none"}))}));
+//# sourceMappingURL=index.b4ed9ec8.js.map
